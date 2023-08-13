ffa97a6ead3e75e31503c302a383e930b2248859
YxMARCKxY <mramirez@ekatena.com>
2023-08-13 17:53:07 -0600

    Add images preload and card render function

--- App.js
+++ App.js
@@ -1,386 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, TextInput, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Image, TextInput, ScrollView, StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
+
+  
+
+  const data = [
+    {key: 1},
+    {key: 2},
+    {key: 3},
+    {key: 4},
+    {key: 5},
+    {key: 6},
+    {key: 7},
+    {key: 8},
+    {key: 9},
+    {key: 10}
+  ]
+
+  const cardStruct = ({item}) => {
+    const images = [
+      require('./assets/profile-avatars/avatar1.png'),
+      require('./assets/profile-avatars/avatar2.png'),
+      require('./assets/profile-avatars/avatar3.png'),
+      require('./assets/profile-avatars/avatar4.png'),
+      require('./assets/profile-avatars/avatar5.png'),
+      require('./assets/profile-avatars/avatar6.png'),
+      require('./assets/profile-avatars/avatar7.png'),
+      require('./assets/profile-avatars/avatar8.png'),
+      require('./assets/profile-avatars/avatar9.png'),
+      require('./assets/profile-avatars/avatar10.png')
+    ];
+
+    return(
+      <View style={styles.card}>
+        <View style={styles.cardHeader}>
+          <Image style={styles.cardHeaderIcon} source={images[item.key-1]}/>
+          <Text style={styles.cardHeaderTitle}>ZEN APP CARD TITLE</Text>
+        </View>
+
+        <View style={styles.cardBody}>
+          <Text>Some text here</Text>
+        </View>
+      </View>  
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Image style={styles.headerIcon} source={require('./assets/icon.png')}/>
-          <View style={styles.searchBarContainer}>
-            <TextInput style={styles.searchBar} placeholder='Buscar'/>
-          </View>
-      </View>
 
+      <View style={styles.header}>
+        <ScrollView keyboardShouldPersistTaps="handled">
+          <View style={styles.logoAndBarContainer}>
+            <Image style={styles.headerIcon} source={require('./assets/icon.png')}/>
+            <View style={styles.searchBarContainer}>
+                <TextInput style={styles.searchBar} placeholder='Buscar'/>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+      
       <View style={styles.content}>
         <ScrollView keyboardShouldPersistTaps="handled">
           <View style={styles.cardContainer}>
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar1.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar2.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar3.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar4.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar5.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar8.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar9.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar10.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar2.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar3.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar4.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar5.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar2.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar3.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar4.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar5.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar2.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar3.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar4.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar5.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar2.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar3.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar4.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar5.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-            <View style={styles.card}>
-              <View style={styles.cardHeader}>
-                <Image style={styles.cardHeaderIcon} source={require('./assets/profile-avatars/avatar6.png')}/>
-                <Text style={styles.cardHeaderTitle}> Title card  :)</Text>
-              </View>
-              <View style={styles.cardBody}>
-                <Text>Text body</Text>
-              </View>
-            </View>
-
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
+            <FlatList data={data} renderItem={cardStruct}/>
           </View>
-
           <StatusBar style="auto" />
-
         </ScrollView>
       </View>
     </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
@@ -401,7 +91,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: '2%'
+    paddingVertical: '2%',
+    paddingRight: '1%'
   },
   searchBar:{
     backgroundColor: '#ebece9',
@@ -432,6 +123,10 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: '0.5%'
   },
+  logoAndBarContainer:{
+    flex: 1,
+    flexDirection: 'row'
+  },
   cardBody:{
     flex: 5,
     flexDirection: 'column'
@@ -444,10 +139,9 @@ const styles = StyleSheet.create({
     flex: 4
   },
   header:{
-    flex: 2,
-    flexDirection: 'row'
+    flex: 2
   },
   content: {
-    flex: 20
+    flex: 21
   }
 });
